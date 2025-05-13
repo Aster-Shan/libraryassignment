b535eb61491cf01c92683ca479ef73296c8d0128
Aster-Shan <thuushanphyoe098@gmail.com>
2025-05-13 21:10:59 +0100

    Deploy React app to GitHub Pages

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -4,15 +4,20 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/libraryassignment/', // Base path for GitHub Pages
 
   server: {
     port: 3000,
-    historyApiFallback: true,
+    historyApiFallback: true, // For handling SPA routing (404 fallback)
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-    }, 
-    }
-  }
+        target: 'http://localhost:8080', // Backend API endpoint for local development
+        changeOrigin: true, // For cross-origin requests during local development
+      },
+    },
+  },
+
+  build: {
+    outDir: 'dist', // Output directory for GitHub Pages deployment
+  },
 })
